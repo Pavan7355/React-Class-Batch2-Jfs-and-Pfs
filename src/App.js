1493a2b1fd6449eae63800e079abd0e4ab342070
pavan s <pavan.s@pentagonspace.in>
2024-11-19 13:41:32 +0530

    component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Header from './Components/Header'
+import Nav from './Components/Nav';
+import Aside from './Components/Aside';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Header/>
+<Nav/>
+<div style={{display:"flex",gap:"20px"}}>
+<Aside/>
+<Main/>
+</div>
+<br/>
+<Footer/>
+</>
   );
 }
 
