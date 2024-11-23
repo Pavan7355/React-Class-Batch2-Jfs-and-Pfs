f4df034e0f7c342d50f10aa66f6c6e51762be660
pavan s <pavan.s@pentagonspace.in>
2024-11-23 13:09:55 +0530

    props assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import ParentComponent from './ReactProps/ParentComponent';
-import Book from './ReactProps/Book';
+import Header from './PropsAssignment1/Header';
+import Footer from './PropsAssignment1/Footer';
+import Card from './PropsAssignment1/Card';
+
 
 function App() {
   return (
 <>
-{/* <ParentComponent/> */}
-<Book/>
+<Header/>
+<Card image="https://documents.iplt20.com/ipl/IPLHeadshot2024/2.png" 
+name="Virat Kholi"/>
+<Footer/>
 </>
   );
 }
