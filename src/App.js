99960e346e6d84166cd18f8c3bbc3963de6043c6
pavan s <pavan.s@pentagonspace.in>
2024-11-25 13:31:58 +0530

    default props and props desturcting passing function inside props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react'
-import Header from './PropsAssignment1/Header';
-import Footer from './PropsAssignment1/Footer';
-import Card from './PropsAssignment1/Card';
-
+import Details from './DefaultProps/Details';
+import Example1 from './PropsDestructing/Example1';
+import Example2 from './PropsDestructing/Example2';
+import ParentComponent from './PropsFunction/ParentComponent';
 
 function App() {
   return (
 <>
-<Header/>
-<Card image="https://documents.iplt20.com/ipl/IPLHeadshot2024/2.png" 
-name="Virat Kholi"/>
-<Footer/>
+{/* <Details name="virat" age="36" city="Bangalore"/>
+<Details name="Sachin" age="27" city="Mangalore"/>
+<Details age="47" city="mysore"/>
+<Details/>
+<Details name="Raj" city="Kolar"/> */}
+{/* <Example1 name="Raj" age="22" city="Bangalore"
+  isStudent={false}/>
+  <Example1 name="viart" age="36" city="Mysore"
+  isStudent={true}/> */}
+  {/* <Example2 name="Raj" age="22" city="Bangalore"
+  isStudent={false}/>
+  <Example2 name="sachin"  age="16"  city="kolar"
+  isStudent={true}/> */}
+  <ParentComponent/>
 </>
   );
 }
