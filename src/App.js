0054e45adad0c708df3f1ec24d23f9f316bc43c3
pavan s <pavan.s@pentagonspace.in>
2024-11-26 13:36:40 +0530

    React state props validation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react'
-import Details from './DefaultProps/Details';
-import Example1 from './PropsDestructing/Example1';
-import Example2 from './PropsDestructing/Example2';
-import ParentComponent from './PropsFunction/ParentComponent';
+import Validation from './PropsValidation/Validation';
+import Example1 from './ReactState/Example1';
+import Example2 from './ReactState/Example2';
 
 function App() {
+// let handleClick= ()=>{
+//     alert("i am arrow function")
+//   }
   return (
 <>
-{/* <Details name="virat" age="36" city="Bangalore"/>
-<Details name="Sachin" age="27" city="Mangalore"/>
-<Details age="47" city="mysore"/>
-<Details/>
-<Details name="Raj" city="Kolar"/> */}
-{/* <Example1 name="Raj" age="22" city="Bangalore"
-  isStudent={false}/>
-  <Example1 name="viart" age="36" city="Mysore"
-  isStudent={true}/> */}
-  {/* <Example2 name="Raj" age="22" city="Bangalore"
-  isStudent={false}/>
-  <Example2 name="sachin"  age="16"  city="kolar"
-  isStudent={true}/> */}
-  <ParentComponent/>
+{/* <Validation name="Raj" age={30} isStudent={false}
+click={handleClick}/> */}
+{/* <Example1/> */}
+<Example2/>
 </>
   );
 }
