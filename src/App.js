ef4aab7a33df98ff4826aa8920c02ce7edb06d0c
pavan s <pavan.s@pentagonspace.in>
2024-12-05 15:36:10 +0530

    React useState hook example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import Example2 from './ReactTable/Example2';
-// import Example from './ReactTable/Example';
+import Example1 from './UseStateHook/Example1';
+import Example2 from './UseStateHook/Example2';
+import Example3 from './UseStateHook/Example3';
+import Example4 from './UseStateHook/Example4';
 
 
 function App() {
 
   return (
 <>
-{/* <Example/> */}
-<Example2/>
+{/* <Example1/> */}
+{/* <Example2/> */}
+{/* <Example3/> */}
+<Example4/>
 </>
   );
 }
