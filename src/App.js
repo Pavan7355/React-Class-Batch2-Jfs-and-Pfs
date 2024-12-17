ebdec9e2fa37154b8f0c3ca0760cf3c1eb6fd2c4
pavan s <pavan.s@pentagonspace.in>
2024-12-17 13:43:46 +0530

    React Router Dom and Axios Example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react'
-import Counter1 from './CustomHook/Counter1';
-import Counter2 from './CustomHook/Counter2';
-
+// import Nav from './Navbar/Nav';
+// import Home from './Navbar/Home';
+// import About from './Navbar/About';
+// import Profile from './Navbar/Profile';
+// import Login from './Navbar/Login';
+// import Signup from './Navbar/Signup';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Login from './Froms/Login'
+import Signup from './Froms/Signup'
+import Password  from './Froms/Password'
+import Axios from './Axios/Axios'
 function App() {
 
   return (
 <>
-<Counter1/>
-<Counter2/>
+
+{/* <BrowserRouter>
+<Nav/>
+<Routes>
+  <Route path="/" element={<Home/>}></Route>
+<Route path='/about' element={<About/>}></Route>
+<Route path='/profile' element={<Profile/>}></Route>
+<Route path='/login' element={<Login/>}></Route>
+<Route path='/signup' element={<Signup/>}></Route>
+</Routes>
+
+</BrowserRouter> */}
+{/* <BrowserRouter>
+<Routes>
+  <Route path='/' element={<Login/>}></Route>
+  <Route path='/signup' element={<Signup/>}></Route>
+  <Route path='/password' element={<Password/>}></Route>
+</Routes>
+</BrowserRouter> */}
+
+
+<Axios/>
 </>
   );
 }
